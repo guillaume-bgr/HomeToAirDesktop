0cbbc39c1388f2a81d27aa2343f54a15bccb121a
guillaume-bgr <guillaumebongrand.lamanu@gmail.com>
2023-07-05 13:34:23 +0200

    First commit

---
+++ src/renderer/src/App.jsx
@@ -0,0 +1,12 @@
+import Versions from './components/Versions'
+import icons from './assets/icons.svg'
+
+function App() {
+  return (
+    <div className="container">
+      <h1>test</h1>
+    </div>
+  )
+}
+
+export default App
