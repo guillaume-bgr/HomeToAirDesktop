0769f4d339802c598dc21d728814366aaf00bbce
guillaume-bgr <guillaumebongrand.lamanu@gmail.com>
2023-07-06 09:28:03 +0200

    Ajout router

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,11 +1,30 @@
 import Versions from './components/Versions'
 import icons from './assets/icons.svg'
+import Home from './pages/Home.jsx'
+import Sidebar from './components/layout/Sidebar'
+import Template from './pages/Template'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Template />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: '/sensors',
+        element: <Sensors />
+      }
+    ]
+  },
+])
 
 function App() {
   return (
-    <div className="container">
-      <h1>test</h1>
-    </div>
+    <RouterProvider router={router}/>
   )
 }
 
