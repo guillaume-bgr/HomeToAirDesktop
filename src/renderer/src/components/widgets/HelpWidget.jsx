f4777c21cdd4976223c436f12729c397fef1d63a
guillaume-bgr <guillaumebongrand.lamanu@gmail.com>
2023-09-07 15:36:32 +0200

    changement ombre helpwidget

--- src/renderer/src/components/widgets/HelpWidget.jsx
+++ src/renderer/src/components/widgets/HelpWidget.jsx
@@ -9,7 +9,7 @@ function HelpWidget(props) {
                 </svg>
                 </span>
             </span>
-            <div className={"help-widget-popup shadow " + props.modalDirection}>
+            <div className={"help-widget-popup " + props.modalDirection}>
                 {props.children}
             </div>
         </div>
